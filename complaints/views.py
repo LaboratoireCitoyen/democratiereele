659d2b0a077c8af6c0cbeb7e500b7140c264d83d
jpic <jamespic@gmail.com>
2015-02-19 12:46:39 +0100

    Added breadcrumbs on complaints list

--- complaints/views.py
+++ complaints/views.py
@@ -1,7 +1,10 @@
 from django.views import generic
 from django.core.urlresolvers import reverse
+from django import http
 
 import rules_light
+
+from cities_light.models import City, Region, Country
 
 from .models import Complaint, Action
 from .forms import ComplaintForm, ActionForm
@@ -35,30 +38,59 @@ class ComplaintDetailView(generic.DetailView):
 
 class ComplaintListView(generic.ListView):
     model = Complaint
+    paginate_by = 10
 
     def get_queryset(self):
         q = Complaint.objects.all().select_related('city', 'city__region',
                 'city__region__country')
 
-        country_slug = self.kwargs.get('country_slug', 'all')
-        region_slug = self.kwargs.get('region_slug', 'all')
-        city_slug = self.kwargs.get('city_slug', 'all')
-        tag = self.kwargs.get('tag', 'all')
+        self.country_slug = self.kwargs.get('country_slug', 'all')
+        self.region_slug = self.kwargs.get('region_slug', 'all')
+        self.city_slug = self.kwargs.get('city_slug', 'all')
+        self.tag = self.kwargs.get('tag', 'all')
 
-        if country_slug != 'all':
-            q = q.filter(city__region__country__slug=country_slug)
+        if self.country_slug != 'all':
+            q = q.filter(city__region__country__slug=self.country_slug)
 
-        if region_slug != 'all':
-            q = q.filter(city__region__slug=region_slug)
+        if self.region_slug != 'all':
+            q = q.filter(city__region__slug=self.region_slug)
 
-        if city_slug != 'all':
-            q = q.filter(city__slug=city_slug)
+        if self.city_slug != 'all':
+            q = q.filter(city__slug=self.city_slug)
 
-        if tag != 'all':
-            q = q.filter(tags__name__in=[tag])
+        if self.tag != 'all':
+            q = q.filter(tags__name__in=[self.tag])
 
         return q
 
+    def get_context_data(self, *args, **kwargs):
+        c = super(ComplaintListView, self).get_context_data(*args, **kwargs)
+        
+        try:
+            self.populate_context(c)
+        except (City.DoesNotExist, Region.DoesNotExist, Country.DoesNotExist):
+            raise http.Http404()
+
+        return c
+
+    def populate_context(self, context):
+        if self.city_slug != 'all':
+            context['city'] = City.objects.get(slug=self.city_slug,
+                region__slug=self.region_slug, 
+                region__country__slug=self.country_slug)
+            context['region'] = context['city'].region
+            context['country'] = context['city'].region.country
+
+        elif self.region_slug != 'all':
+            context['region'] = Region.objects.get(slug=self.region_slug,
+                    country__slug=self.country_slug)
+            context['country'] = context['region'].country
+
+        elif self.country_slug != 'all':
+            context['country'] = Country.objects.get(slug=self.country_slug)
+
+        if self.tag != 'all':
+            context['tag'] = self.tag
 
 @rules_light.class_decorator
 class ActionDetailView(generic.DetailView):
