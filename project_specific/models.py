4e5fe7651d65638c84ef0f524191614901f9fbed
jpic <jamespic@gmail.com>
2015-02-19 13:10:17 +0100

    Select related user and accounts with comments

--- project_specific/models.py
+++ project_specific/models.py
@@ -4,5 +4,11 @@ from decision.models import Poll
 from threadedcomments.models import ThreadedComment
 
 
+class ProjectCommentManager(CommentManager):
+    def get_queryset(self):
+        return super(ProjectCommentManager, self).get_queryset(
+                ).select_related('user', 'user__account')
+
+
 class ProjectComment(ThreadedComment, Poll):
-    objects = CommentManager()
+    objects = ProjectCommentManager()
